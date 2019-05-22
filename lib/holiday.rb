47a023ff736982a85808be9c7bd3d24f76d7aae1
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 09:09:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |name, supply|
       if supply.include?("BBQ")
-        array << name
+        arr << name
       end
     end
   end
