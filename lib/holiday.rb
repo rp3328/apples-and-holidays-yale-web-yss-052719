08d41a3cb6fbe19660a44ed7adcd038f47dd8df1
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 09:13:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
 
       name = arr.join(" ") #array to string
       supply = supply.join(", ")
-      puts " #{name}: #{supply}"
+      puts "  #{name}: #{supply}"
     end
   end
 end
